bb35a7140488ce978d3d51920d506c481e0603c3
jobytchacko@gmail.com <joby123>
2014-07-07 10:22:52 +0530

    Changes in ilids dataAccessServlet for getting data for graph. Created a final class ServerConfig to place common variable

---
+++ src/main/java/com/ilids/conf/ServerConfig.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ilids.conf;
+
+/**
+ *
+ * @author cirakas
+ */
+public final class ServerConfig {
+    public static String DB_TYPE = "jdbc:mysql:";
+    public static String DB_NAME = "ilids";
+    public static String DB_HOSTNAME = "localhost";
+    public static String DB_PORT = "3306";
+    public static String DB_USERNAME = "root";
+    public static String DB_PASS = "password";
+   
+}
--- src/main/java/com/ilids/servlet/DataAccessServlet.java
+++ src/main/java/com/ilids/servlet/DataAccessServlet.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.ilids.servlet;
 
+import com.ilids.conf.ServerConfig;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
 import java.io.IOException;
@@ -40,59 +40,51 @@ public class DataAccessServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 	    throws ServletException, IOException, SQLException {
-	
-                System.out.println("CInside the servlets-----");
-                response.setContentType("application/json");
-                String connectionUrl="jdbc:mysql://localhost:3306/ilids";
-                String dbUserName="root";
-                String dbPassword="password";
-                Connection connection = (Connection) DriverManager.getConnection(connectionUrl,dbUserName,dbPassword);
-                Statement statement=(Statement) connection.createStatement();
-                String selectQuery="SELECT time as data_time , data as real_data FROM data WHERE `time` BETWEEN '2014-06-29' AND '2014-07-21'    and address_map=6";
-                ResultSet rs=statement.executeQuery(selectQuery);
-        PrintWriter out = response.getWriter();
-       // JSONObject json = new JSONObject();
-        
-        JSONArray jsonArray=new JSONArray();
+	response.setContentType("application/json");
+	String connectionUrl=ServerConfig.DB_TYPE+"//"+ServerConfig.DB_HOSTNAME+":"+ServerConfig.DB_PORT+"/"+ServerConfig.DB_NAME;
+	String dbUserName = ServerConfig.DB_USERNAME;
+	String dbPassword = ServerConfig.DB_PASS;
+	Connection connection = (Connection) DriverManager.getConnection(connectionUrl, dbUserName, dbPassword);
+	Statement statement = (Statement) connection.createStatement();
+	String selectQuery = "SELECT time as data_time , data as real_data FROM data WHERE `time` BETWEEN '2014-06-29' AND '2014-07-21'    and address_map=6";
+	ResultSet rs = statement.executeQuery(selectQuery);
+	PrintWriter out = response.getWriter();
+
+	JSONArray jsonArray = new JSONArray();
 	try {
 	    /* TODO output your page here. You may use following sample code. */
-               // jsonArray=dataService.getAllData();
-            // int count = 5;
-            float datas=1; 
-            String realDate="";
-             String pattern = "MM/dd/yyyy HH:mm:ss";
-            SimpleDateFormat  format = new SimpleDateFormat(pattern);
-                    while(rs.next()){
-                        datas=rs.getFloat("real_data");
-                        realDate=format.format(rs.getTimestamp("data_time"));
-                        System.out.println("Date is----"+realDate);
-                        System.out.println("select----+++"+datas);
-                        JSONObject json = new JSONObject();
-                        json.put("date",realDate);
-                        json.put("current",datas);
-                        json.put("power",datas);
-                        json.put("voltage",datas);
-                        jsonArray.put(json); 
-                        
-                    }
-                      System.out.println("select----+++"+jsonArray.toString());
-            out.println(jsonArray.toString());
-            if(rs!=null){
-                rs.close();
-            }
-            if(connection!=null){
-                connection.close();
-            }
-        }catch(Exception e){
-            e.printStackTrace();
+	    float datas = 1;
+	    String realDate = "";
+	    String pattern = "MM/dd/yyyy HH:mm:ss";
+	    SimpleDateFormat format = new SimpleDateFormat(pattern);
+	    while (rs.next()) {
+		datas = rs.getFloat("real_data");
+		realDate = format.format(rs.getTimestamp("data_time"));
+		JSONObject json = new JSONObject();
+		json.put("date", realDate);
+		json.put("current", datas);
+		json.put("power", datas);
+		json.put("voltage", datas);
+		jsonArray.put(json);
+
+	    }
+	    out.println(jsonArray.toString());
+	    if (rs != null) {
+		rs.close();
+	    }
+	    if (connection != null) {
+		connection.close();
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace();
 	} finally {
 	    out.close();
-            if(connection!=null){
-                connection.close();
-            }
-             if(rs!=null){
-                rs.close();
-            }
+	    if (connection != null) {
+		connection.close();
+	    }
+	    if (rs != null) {
+		rs.close();
+	    }
 	}
     }
 
@@ -108,11 +100,11 @@ public class DataAccessServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	    throws ServletException, IOException {
-                       try {
-                           processRequest(request, response);
-                       } catch (SQLException ex) {
-                           Logger.getLogger(DataAccessServlet.class.getName()).log(Level.SEVERE, null, ex);
-                       }
+	try {
+	    processRequest(request, response);
+	} catch (SQLException ex) {
+	    Logger.getLogger(DataAccessServlet.class.getName()).log(Level.SEVERE, null, ex);
+	}
     }
 
     /**
@@ -126,11 +118,11 @@ public class DataAccessServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
 	    throws ServletException, IOException {
-                       try {
-                           processRequest(request, response);
-                       } catch (SQLException ex) {
-                           Logger.getLogger(DataAccessServlet.class.getName()).log(Level.SEVERE, null, ex);
-                       }
+	try {
+	    processRequest(request, response);
+	} catch (SQLException ex) {
+	    Logger.getLogger(DataAccessServlet.class.getName()).log(Level.SEVERE, null, ex);
+	}
     }
 
     /**
