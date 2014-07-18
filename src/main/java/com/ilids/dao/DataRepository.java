38735e75e86eec4d51622bf82fdc8c8bf9cf5e9e
jobytchacko@gmail.com <joby123>
2014-07-18 09:40:51 +0530

    Range changed for alert system

--- src/main/java/com/ilids/dao/DataRepository.java
+++ src/main/java/com/ilids/dao/DataRepository.java
@@ -14,7 +14,7 @@ public class DataRepository extends AbstractGenericDao<Data> {
     
 
     public List<Data> getAllAlertData(double mdv) {
-        return (List<Data>)entityManager.createQuery("SELECT u FROM Data u where data > "+mdv+" and (address_map=12 or address_map=14 or address_map=16)").getResultList();
+        return (List<Data>)entityManager.createQuery("SELECT u FROM Data u where time BETWEEN '2014-07-10 00:00:01' AND '2014-07-29 23:59:59' and data > "+mdv+" and (address_map=12 or address_map=14 or address_map=16)").getResultList();
     }
 //    public List<Data> getAllData() {
 //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
